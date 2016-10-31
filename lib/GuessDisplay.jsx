cbd7351bbd1acbe22bb9c540336b46818fa4097a
Maia Stone <maiadov@gmail.com>
2016-10-30 22:37:12 -0600

    generates random number

--- lib/GuessDisplay.jsx
+++ lib/GuessDisplay.jsx
@@ -1,27 +1,29 @@
-const React = require('react');
-
-
-
-class GuessDisplay extends React.Component {
-
-
-// let result;
-//   if (this.props.lastGuess > this.props.computerNumber) {
-//     result = 'too high';
+// const React = require('react');
+//
+//
+//
+// class GuessDisplay extends React.Component {
+//   constructor() {
+//     super()
 //   }
-
-
-render() {
-  return (
-    <div>
-      <h2>Your last guess was {this.props.lastGuess}</h2>
-      <h3>That number is {} - Guess again. </h3>
-    </div>
-  );
-}
-
-
-
-}
-
-module.exports = GuessDisplay;
+//
+//
+// // let result;
+// //   if (this.props.lastGuess > this.props.computerNumber) {
+// //     result = 'too high';
+// //   }
+//
+//
+// render() {
+//   return (
+//     <div>
+//
+//     </div>
+//   );
+// }
+//
+//
+//
+// }
+//
+// module.exports = GuessDisplay;
--- lib/input.jsx
+++ lib/input.jsx
@@ -5,33 +5,54 @@ class GuessInputFields extends React.Component {
   constructor() {
     super();
     this.state = {
-      guess: ''
+      lastGuess: '',
+      guess: '',
+      computerNumber: ''
     }
   }
 
+  generateRandomNumber(min, max){
+    return Math.floor(Math.random() * (max-min + 1)) + min;
+  }
 
   handleClick() {
+    let randomNumber = this.generateRandomNumber(0, 100);
     this.setState({lastGuess: this.state.guess});
+    this.setState({guess:''});
+    this.setState({computerNumber: {randomNumber} });
+  }
+
+  clearInput() {
     this.setState({guess:''});
   }
 
+  resetGame() {
+    this.setState({
+      computerNumber: '',
+      lastGuess: '',
+      guess: ''
+    })
+  }
+
+  // compareGuess() {
+  //   this.state.guess > this.state.computerNumber.randomNumber ? 'too high' : 'too low' }
 
   render() {
     return (
       <div>
+        <h2>your last guess was {this.state.lastGuess}</h2>
+        <h3>your guess was {}!</h3>
         <input type='number' value={this.state.guess}
             placeholder='guess a number'
             onChange={(e)=>this.setState({guess: parseInt(e.target.value, 10)})} />
         <button onClick={()=>this.handleClick()}>Guess</button>
-        <button onClick={()=>this.setState({guess:''})}>Clear</button>
-        <button onClick={()=>this.handleClick()}>Reset</button>
+        <button onClick={()=>this.clearInput()}>Clear</button>
+        <button onClick={()=>this.resetGame()}>Reset</button>
 
       </div>
     );
   }
-
-
-
 }
+
 
 module.exports = GuessInputFields;
