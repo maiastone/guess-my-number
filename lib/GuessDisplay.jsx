1f7e699a6da405dd4c165c837237c620ca7f4f5f
Maia Stone <maiadov@gmail.com>
2016-10-30 20:05:56 -0600

    WIP break out components

--- lib/GuessDisplay.jsx
+++ lib/GuessDisplay.jsx
@@ -1,14 +1,22 @@
 const React = require('react');
-const guess = require('./input');
 
-class GuessField extends React.Component {
 
+
+class GuessDisplay extends React.Component {
+
+
+// let result;
+//   if (this.props.lastGuess > this.props.computerNumber) {
+//     result = 'too high';
+//   }
 
 
 render() {
   return (
-
-
+    <div>
+      <h2>Your last guess was {this.props.lastGuess}</h2>
+      <h3>That number is {} - Guess again. </h3>
+    </div>
   );
 }
 
@@ -16,4 +24,4 @@ render() {
 
 }
 
-module.exports = GuessField;
+module.exports = GuessDisplay;
--- lib/input.jsx
+++ lib/input.jsx
@@ -1,18 +1,30 @@
 const React = require('react');
 
 
-class GuessInputField extends React.Component {
+class GuessInputFields extends React.Component {
   constructor() {
     super();
-
+    this.state = {
+      guess: ''
+    }
   }
 
+
+  handleClick() {
+    this.setState({lastGuess: this.state.guess});
+    this.setState({guess:''});
+  }
 
 
   render() {
     return (
       <div>
-
+        <input type='number' value={this.state.guess}
+            placeholder='guess a number'
+            onChange={(e)=>this.setState({guess: parseInt(e.target.value, 10)})} />
+        <button onClick={()=>this.handleClick()}>Guess</button>
+        <button onClick={()=>this.setState({guess:''})}>Clear</button>
+        <button onClick={()=>this.handleClick()}>Reset</button>
 
       </div>
     );
@@ -22,4 +34,4 @@ class GuessInputField extends React.Component {
 
 }
 
-module.exports = GuessInputField;
+module.exports = GuessInputFields;
