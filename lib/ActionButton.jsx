2d2f806e2ed8cda9752753d08d0565e68585178e
Maia Stone <maiadov@gmail.com>
2016-11-01 17:00:03 -0600

    WIP refactor ActionButton

---
+++ lib/ActionButton.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+
+class ActionButton extends React.Component {
+
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+module.exports = ActionButton;
